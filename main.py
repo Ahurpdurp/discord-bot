16e11877f712d2896b7c8486ff914980938cf2fd
Paul Wu <j.paul.wu@gmail.com>
2019-06-13 19:00:33 -0500

    adding some bug fixes

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def on_message(message):
         else:
             await message.channel.send("Hmm, " + account_name + " isn\'t ranked in solo/duo right now. There might be some results from earlier seasons though.")
         await message.channel.send("Loading results...")
-        match_history_url = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + account_id + '?queue=420&endIndex=50&api_key=' + key
+        match_history_url = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + account_id + '?queue=420&endIndex=90&api_key=' + key
         match_history_data = requests.get(match_history_url).json()
         match_id_list = []
         for match in match_history_data['matches']:
@@ -54,6 +54,11 @@ async def on_message(message):
             match_url = 'https://na1.api.riotgames.com/lol/match/v4/matches/' + str(match_id) + '?api_key=' + key
             match_details = requests.get(match_url).json()
             team_side = []
+            try:
+                match_details['participantIdentities']
+            except:
+                await message.channel.send("Unable to get the recent player's matches. Most likely the API call limit was reached, so wait at least two minutes and try again.")
+                return 
             for player in match_details['participantIdentities']:
                 if player['player']['accountId'] == account_id:
                     if player['participantId'] in [1,2,3,4,5]:
@@ -66,7 +71,7 @@ async def on_message(message):
                     player_list[player_name] = player_list.get(player_name,0) + 1
         edited_player_list = player_list.copy()
         for player_key in player_list:
-            if player_list[player_key] == 1:
+            if player_list[player_key] in [1,2]:
                 del edited_player_list[player_key]
         ordered_player_list = sorted(edited_player_list.items(), key=lambda x: x[1], reverse=True)  
         if len(ordered_player_list) == 0:
@@ -80,7 +85,8 @@ async def on_message(message):
             await message.channel.send(f'Overall, {account_name} has duoed 100% of his/her games.')
             return
         await message.channel.send(f'Overall, {account_name} has duoed {round(100 * total_duoed_games/len(match_id_list), 2)}% of his/her games.')
-
+        await message.channel.send('Disclaimer: a person only shows up if they duoed more than 2 games. There\'s also a chance (especially in higher elos) that some people who show up are just random players \
+who happened to be on the same team multiple times.')
     
 @client.event
 async def on_ready():
