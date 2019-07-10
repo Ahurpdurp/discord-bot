c476ddb31c1d25d492df9280c9ac449efe83038e
Paul Wu <j.paul.wu@gmail.com>
2019-07-09 20:00:19 -0500

    wintrading feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json, urllib.request, requests
 from ratelimit import limits, sleep_and_retry
 from ranked import ranked_search
 from normals import normals_search
+from win_trade import win_trade_search
 import os
 
 
@@ -23,13 +24,13 @@ async def on_message(message):
         return
     author_id = message.author
     if message.content == '!rules':
-        await message.channel.send('Type !rank, !flex, !norm, or !aram followed by a summoner name to search their history and see who that person\'s \
+        await message.channel.send('Type !rank, !flex, !norm, !wint (wintrade) or !aram followed by a summoner name to search their history and see who that person\'s \
 played with for the last 90 games of the specified game mode.')
         return  
     if message.content.startswith('!ranked'):
         await message.channel.send('Remember, start with !rank, not !ranked. Try again :)')
         return  
-    if message.content.startswith('!rank') or message.content.startswith('!norm') or message.content.startswith('!flex') or message.content.startswith('!aram'):
+    if message.content.startswith('!wint') or message.content.startswith('!rank') or message.content.startswith('!norm') or message.content.startswith('!flex') or message.content.startswith('!aram'):
         account_name = message.content[6:]        
         sum_id_URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ account_name + '?api_key=' + key 
         try:
@@ -41,6 +42,8 @@ played with for the last 90 games of the specified game mode.')
             return
         if message.content.startswith('!rank'):
             await ranked_search(account_name,account_id,summoner_id,key,message)
+        elif message.content.startswith('!wint'):
+            await win_trade_search(account_name,account_id,summoner_id,key,message)
         elif message.content.startswith('!norm') or message.content.startswith('!flex') or message.content.startswith('!aram'):
             await normals_search(account_name,account_id,summoner_id,key,message,message.content[1:5])
 @client.event
---
+++ win_trade.py
@@ -0,0 +1,53 @@
+import discord
+import json, urllib.request, requests
+from ratelimit import limits, sleep_and_retry
+
+async def win_trade_search(account_name,account_id,summoner_id,key,message):
+    player_ranking_URL = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ summoner_id +"?api_key=" + key 
+    player_ranking_info = requests.get(player_ranking_URL).json()
+    for queue_type in player_ranking_info:
+        if queue_type['queueType'] == 'RANKED_SOLO_5x5':
+            await message.channel.send(account_name + f'\'s current rank is: {queue_type["tier"].capitalize()} {queue_type["rank"]}')
+            break
+    else:
+        await message.channel.send("Hmm, " + account_name + " isn\'t ranked in solo/duo right now. There might be some results from earlier seasons though.")
+    await message.channel.send("Loading results...")
+    match_history_url = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + account_id + '?queue=420&endIndex=80&api_key=' + key
+    match_history_data = requests.get(match_history_url).json()
+    match_id_list = []
+    for match in match_history_data['matches']:
+        match_id_list.append(match['gameId'])
+    player_list = {}
+    for match in match_id_list:
+        match_id = match
+        match_url = 'https://na1.api.riotgames.com/lol/match/v4/matches/' + str(match_id) + '?api_key=' + key
+        match_details = requests.get(match_url).json()
+        team_side = []
+        try:
+            match_details['participantIdentities']
+        except:
+            await message.channel.send("Unable to get the recent player's matches. Most likely the API call limit was reached, so wait at least two minutes and try again.")
+            return 
+        for player in match_details['participantIdentities']:
+            if player['player']['accountId'] == account_id:
+                if player['participantId'] in [1,2,3,4,5]:
+                    team_side = [5,6,7,8,9]
+                else:
+                    team_side = [0,1,2,3,4]
+        for participantId in team_side:
+            if match_details['participantIdentities'][participantId]['player']['accountId'] != account_id:
+                player_name = match_details['participantIdentities'][participantId]['player']['summonerName']
+                player_list[player_name] = player_list.get(player_name,0) + 1
+    edited_player_list = player_list.copy()
+    for player_key in player_list:
+        if player_list[player_key] in [1,2]:
+            del edited_player_list[player_key]
+    ordered_player_list = sorted(edited_player_list.items(), key=lambda x: x[1], reverse=True)  
+    if len(ordered_player_list) == 0:
+        await message.channel.send("This player probably hasn't wintraded in the past 85 games.")
+        return
+    total_duoed_games = 0
+    for x in ordered_player_list:
+        await message.channel.send(f'Summoner Name: {x[0]} | Total Games Played Against: {x[1]} | Percent Played Against This Person: {round(((x[1]/len(match_id_list)) * 100),2)}%')
+        total_duoed_games += x[1]
+    await message.channel.send('Disclamer: wintrading is a touchy subject. There\'s a chance they could\'ve just played against the same person a lot.')
